3df8cdaab4c176faf04a43a0b6434bf1c5c78c8d
Arpanjot-Singh <108647459+Arpanjot-Singh@users.noreply.github.com>
2023-05-13 05:05:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React,{useState,useEffect} from 'react';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+
+import Products from './components/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/CheckoutForm/Checkout/Checkout';
+
+import {commerce} from './lib/commerce';
+
+const App = () => {
+  const [products,setProducts]=useState([]);
+  const [cart,setCart]=useState({});
+  const [order,setOrder]=useState({});
+  const [errorMessage,setErrorMessage]=useState('');
+
+
+  const fetchProducts =async() =>{
+    const {data} =await commerce.products.list();
+    setProducts(data);
+  }
+  const fetchCart =async() =>{
+    const cart =await commerce.cart.retrieve();
+    setCart(cart);
+  }
+
+  const handleAddToCart=async(productId,quantity)=>{
+    const item=await commerce.cart.add(productId,quantity);
+    setCart(item);
+  }
+
+  const handleUpdateCartQty=async(productId,quantity)=>{
+    const item=await commerce.cart.update(productId,{quantity});
+    setCart(item)
+  }
+
+  const handleRemoveFromCart=async(productId)=>{
+    const item=await commerce.cart.remove(productId);
+    setCart(item)
+  }
+
+  const handleEmptyCart=async()=>{
+    const item=await commerce.cart.empty();
+    setCart(item)
+  }
+
+  const refreshCart= async()=>{
+    const newCart=await commerce.cart.refresh();
+    setCart(newCart); 
+  }
+  const handleCaptureCheckout=async(checkoutTokenId,newOrder)=>{
+    try{
+      const incomingOrder=await commerce.checkout.capture(checkoutTokenId,newOrder);
+      setOrder(incomingOrder);
+      refreshCart();
+    }catch(error){
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
+  useEffect(() =>{ 
+    fetchProducts();
+    fetchCart();
+  },[])
+
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route  path="/" element={<Products products={products} onAddToCart={handleAddToCart}/>}/>
+          <Route  path="/cart" 
+          element={<Cart 
+            cart={cart}
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}
+          />} />
+          <Route  path="/checkout" element={<Checkout cart={cart}  order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>}/>
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export {deafult as Navbar} from './Navbar/Navbar';
+export {deafult as Products} from './Products/Products';
